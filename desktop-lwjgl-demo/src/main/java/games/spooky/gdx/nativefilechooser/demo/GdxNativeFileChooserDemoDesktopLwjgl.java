05b5eee9eb239e419958aa949be0da2ebedc6fde
Thorthur Planté <tuturtemple@gmail.com>
2023-07-14 12:30:31 +0200

    Add demo application for desktop-lwjgl

---
+++ desktop-lwjgl-demo/src/main/java/games/spooky/gdx/nativefilechooser/demo/GdxNativeFileChooserDemoDesktopLwjgl.java
@@ -0,0 +1,37 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Spooky Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package games.spooky.gdx.nativefilechooser.demo;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import games.spooky.gdx.nativefilechooser.desktop.DesktopFileChooser;
+
+public class GdxNativeFileChooserDemoDesktopLwjgl {
+	public static void main(String[] args) {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("gdx-nativefilechooser demo (desktop-lwjgl)");
+		configuration.setWindowedMode(1200, 800);
+		new Lwjgl3Application(new GdxNativeFileChooserDemo(new DesktopFileChooser()), configuration);
+	}
+}
